fcc555a77f58ad6464b3f11314d778d75bee5afb
JuliaHwang <qufskan9396@gmail.com>
2017-08-01 00:13:49 +0900

    migrate few changes to the Music model

--- django_app/music/models.py
+++ django_app/music/models.py
@@ -1,62 +1,3 @@
-# from django.db import models
-# from config import settings
-#
-# __all__ = (
-#     'Music',
-# )
-#
-#
-# # 전체 음악파일/정보 모델
-# class Music(models.Model):
-#     WEATHER_CHOICES = (
-#         ('sunny', '맑음'),
-#         ('cloudy', '흐림'),
-#         ('rainy', '비'),
-#         ('snowy', '눈'),
-#         ('foggy', '안개'),
-#     )
-#     name_author = models.ForeignKey(User, on_delete=models.CASCADE)
-#     img_music = models.ImageField(upload_to='img_music', blank=True)
-#     name_music = models.CharField(max_length=100)
-#     name_singer = models.CharField(max_length=100)
-#     file_music = models.FileField(upload_to='music')
-#     name_album = models.CharField(max_length=100, blank=True)
-#     date_created = models.DateTimeField(auto_now_add=True)
-#     tag_weather = models.ManyToManyField(
-#         'Weather',
-#         through='WeatherTag',
-#         related_name='+',
-#         choices=WEATHER_CHOICES,
-#     )
-#
-#     def __str__(self):
-#         return self.name_music
-#
-#
-# # 날씨 분류
-# class WeatherTag(models.Model):
-#     tag_music = models.ForeignKey(Music, on_delete=models.PROTECT)
-#     tag_weather = models.ForeignKey("Weather", on_delete=models.PROTECT)
-#     sunny = models.IntegerField(verbose_name='맑음', default=1)
-#     foggy = models.IntegerField(verbose_name='안개', default=1)
-#     rainy = models.IntegerField(verbose_name='비', default=1)
-#     cloudy = models.IntegerField(verbose_name='흐림', default=1)
-#     snowy = models.IntegerField(verbose_name='눈', default=1)
-#
-#     class Meta:
-#         unique_together = (
-#             ('tag_music', 'tag_weather'),
-#         )
-#
-#
-# # 사용자의 위치를 받아와 날씨정보 ()시간마다 DB에 업데이트
-# class Weather(models.Model):
-#     latitude = models.FloatField(verbose_name='위도')
-#     longitude = models.FloatField(verbose_name='경도')
-#     location = models.CharField(max_length=100)
-#     time_range = models.DateTimeField(auto_created=True)
-#     name_area = models.CharField(max_length=100)
-#     weather = models.CharField(max_length=100)
 import os
 from forecastiopy import *
 import requests
@@ -153,14 +94,12 @@ class Weather(models.Model):
         import time
         # TODO 요청을 보낸 시간만 추출하여 1시간이 지났으면 날씨정보 업데이트
         cur_hour = time.gmtime(current.time).tm_hour
-        if abs(cur_hour - self.time_range.hour) > 1:
+        if abs(cur_hour - self.time_saved.hour) > 1:
             self.get_weather_info()
         cur_icon = current.icon
         self.weather = cur_icon
         self.weather.save()
         return cur_icon
-
-
 
 
 # 유저별 플레이리스트 모델
